26efffc4cae75dba33a8c8a5ba1fde0a47d1740f
toshi0622 <toshi19930622@gmail.com>
2021-09-29 14:40:24 +0900

    商品詳細表示_1回目

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
   
-  resources :items, only: [:new, :create, :index]
+  resources :items, only: [:new, :create, :index, :show]
   
 end
